6ae523af7f57cfb1d65a59ee569c50bd2acc5efb
Michael <mikechansas@gmail.com>
2021-12-01 22:29:24 +0000

    Implemet client file

---
+++ client.js
@@ -0,0 +1,21 @@
+const net = require('net');
+
+// establishes a connection with the game server
+
+const connect = function () {
+    const conn = net.createConnection({
+      host: '10.0.2.15',
+      port: 50541
+    });
+  
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+  
+    conn.on("data", (message) => {
+      console.log(`Received message: ${message}`);
+    });
+  
+    return conn;
+  };
+
+  module.exports = connect;
--- play.js
+++ play.js
@@ -1,21 +1,22 @@
-const net = require("net");
+const net = require('net');
+const connect = require('./client');
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '10.0.2.15',
-    port: 50541
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: '10.0.2.15',
+//     port: 50541
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
 
-  conn.on("connect", () => {
-    console.log('You are connected to the server')
-  });
+//   conn.on("data", (message) => {
+//     console.log(`Received message: ${message}`);
+//   });
 
-  return conn;
-};
+//   return conn;
+// };
 
 
 
